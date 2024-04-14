a84683f95b8da23b6110bd5723480aa86b65e5fa
Cuong Quoc Dang <84326038+cuongquocdang@users.noreply.github.com>
2024-04-14 11:05:02 +0700

    feat(kafka-producer): log producer

--- kafka-producer/src/main/java/com/example/kafkaproducer/KafkaProducer.java
+++ kafka-producer/src/main/java/com/example/kafkaproducer/KafkaProducer.java
@@ -3,25 +3,47 @@ package com.example.kafkaproducer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducer {
 
     private final ObjectMapper objectMapper;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private static final String PRODUCE_TOPIC = "USER-TOPIC";
+
     public void produce(final User user) {
 
         var parsedJson = parseToJsonString(user);
 
-        kafkaTemplate.send("USER-TOPIC", parsedJson);
+        var resultFuture = kafkaTemplate.send(PRODUCE_TOPIC, parsedJson);
+        // add callback
+        resultFuture.whenComplete((result, throwable) -> {
+            if (throwable == null) {
+                logSuccessProducer(result.getRecordMetadata());
+            } else {
+                logErrorProducer(throwable);
+            }
+        });
     }
 
     @SneakyThrows
     private String parseToJsonString(final User user) {
         return objectMapper.writeValueAsString(user);
+    }
+
+    private static void logSuccessProducer(final RecordMetadata recordMetadata) {
+        log.info("[KAFKA-PRODUCER] Sent message successfully to topic={}, partition={}, offset={}",
+                PRODUCE_TOPIC, recordMetadata.partition(), recordMetadata.offset());
+    }
+
+    private static void logErrorProducer(final Throwable throwable) {
+        log.error("[KAFKA-PRODUCER] Produce message failed", throwable);
     }
 }
